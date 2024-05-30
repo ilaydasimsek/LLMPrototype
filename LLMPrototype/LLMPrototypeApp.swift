85ae0027fa7e7cb894ac390ff843e7dfc08cb4ae
ilaydasimsek <ilaydasimsek4@gmail.com>
2024-05-30 19:20:53 +0300

    Initialize project

---
+++ LLMPrototype/LLMPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMPrototypeApp.swift
+//  LLMPrototype
+//
+//  Created by İlayda Şimşek on 30.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LLMPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
